b21997e0b1c3958275312f71b1ee423425de148a
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-04-23 00:31:17 +0000

    Still working

--- src/models.py
+++ src/models.py
@@ -4,6 +4,13 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import Column, Integer, String, Float, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from typing import List
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped
+from typing import List
 
 Base = declarative_base()
 
@@ -30,3 +37,46 @@ class Address(Base):
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+user_planet_favorite = Table(
+    "user_planet_favorite", 
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("user.id")),
+)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+Base = declarative_base()
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
+    id = Column(Integer, primary_key=True)
+    population = Column(Integer, nullable=False)
+    climate = Column(String, nullable=False)
+    surface_water = Column(Float, nullable=False)
+    gravity = Column(Float, nullable=False)
+
+    user_planet_favorite = Table(
+        "user_planet_favorite", 
+        Base.metadata,
+        Column("user_id", Integer, ForeignKey("user.id")),
+        Column("planet_id", Integer, ForeignKey("planet.id"))
+    )
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    climate = Column(String, nullable=False)
+    surface_water = Column(Float, nullable=False)
+    gravity = Column(Float, nullable=False)
+    favorites: Mapped[List["User"]] = relationship("User", secondary=user_planet_favorite)
+    surface_water = Column(Float, nullable=False)
+    gravity = Column(Float, nullable=False)
+    favorites: Mapped[List [User]] = relationship("User", secondary=user_planet_favorite)
+    
